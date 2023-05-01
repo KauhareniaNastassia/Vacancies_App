8a37b3650275c46581a410dfc90cd8c65f40ad0f
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-05-01 15:49:54 +0300

    feat: inputSearch

--- src/components/searchPage/searchPage.tsx
+++ src/components/searchPage/searchPage.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {FilterBar} from "../../features/filterBar/filterBar";
 import {VacanciesList} from "../../features/vacanciesList/vacanciesList";
 import {InputSearch} from "../../features/inputSearch/InputSearch";
 import {Pagination} from "../pagination/pagination";
+import css from './searchPage.module.scss'
 
 
 export const SearchPage: React.FC = () => {
 
+    const [searchValue, setSearchValue] = useState<string>('');
+
     return (
-        <section>
+        <section className={css.searchPage__wrapper}>
+
          <FilterBar/>
 
-            <div>
-                <InputSearch/>
+            <div className={css.searchPage__content_wrapper}>
+                <InputSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
                 <VacanciesList/>
                 <Pagination/>
             </div>
--- src/features/inputSearch/InputSearch.tsx
+++ src/features/inputSearch/InputSearch.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import {Button} from "../../common/button/button";
+import css from './inputSearch.module.scss'
+import searchIcon from '../../assets/img/searchIcon.svg'
 
+type InputSearchPropsType = {
+    searchValue: string,
+    setSearchValue: (value: string) => void
+}
 
-export const InputSearch: React.FC = () => {
+export const InputSearch: React.FC<InputSearchPropsType> = ({searchValue, setSearchValue}) => {
 
     return (
-        <section>
+        <section className={css.inputSearch__wrapper}>
+
+            <div className={css.inputSearch__input_wrapper}>
+                <img src={searchIcon} alt='search icon'/>
+                <input
+                    className={css.inputSearch__input}
+                    value={searchValue}
+                    placeholder="Введите название вакансии"
+                    onChange={(event) => setSearchValue(event.target.value)}
+                />
+            </div>
+
+
+            <Button title='Поиск'/>
 
         </section>
     )
